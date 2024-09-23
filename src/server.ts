8ee3b03c9fa48d9a56a0f4b1bae648c961defdc5
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-23 12:29:59 +0100

    Install types and call middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,50 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
+import passport from "passport";
 dotenv.config();
+
+const PORT = process.env.PORT || 5400;
 
 const app = express();
 
+// Logger
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 } else {
   app.use(morgan("combined"));
 }
 
-// Middleware
+// Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Example route
+// Cookies
+app.use(cookieParser());
+
+// Basic security
+app.use(helmet());
+
+// Cors
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// Passport / Auth
+app.use(passport.initialize());
+
+// Routes (example for now)
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript with Express!");
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
+// Connect to DB and start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
