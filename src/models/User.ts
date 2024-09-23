9fb8beb42cb497c593ddd669c31ede429a25ecf4
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-23 13:22:19 +0100

    Write initial User model

---
+++ src/models/User.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    password: {
+      type: String,
+      select: false,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please add a name"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    events: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Event",
+      },
+    ],
+    connections: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    connectionId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    provider: {
+      type: String,
+      enum: ["google", "local"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
