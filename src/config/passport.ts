10f74f3eb24669354d77a21d1b0da6cd37a08728
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-10-26 12:10:04 +0100

    Fix minor local strategy bug

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -84,7 +84,7 @@ passport.use(
     { usernameField: "email", passwordField: "password" },
     async (email, password, done) => {
       try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select("+password");
         if (!user) {
           return done(null, false, { message: "Invalid email or password" });
         }
