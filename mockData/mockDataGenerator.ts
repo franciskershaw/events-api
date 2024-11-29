1f9e2505d9f0ec3567000602a1bf89b3fc4eb1b1
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-11-29 11:07:28 +0000

    Make mongo_URI in mock data more type safe

--- mockData/mockDataGenerator.ts
+++ mockData/mockDataGenerator.ts
@@ -20,7 +20,10 @@ const generateMockEvents = async (
     }`
   );
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
 
     const user = await User.findOne({ email: userEmail });
