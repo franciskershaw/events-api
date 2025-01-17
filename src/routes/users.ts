4017d8da86b2e7caac02153dc2ae32b9123c8c3b
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2025-01-17 10:43:03 +0000

    Fix ts issues

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,20 +1,10 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import { authenticateToken } from "../middleware/authMiddleware";
-import {
-  connectUsers,
-  getUserInfo,
-  removeConnection,
-} from "../controllers/users";
+import { getUserInfo } from "../controllers/users";
 
 const router = express.Router();
 
 router.get("/", authenticateToken, asyncHandler(getUserInfo));
-router.post("/connections", authenticateToken, asyncHandler(connectUsers));
-router.delete(
-  "/connections",
-  authenticateToken,
-  asyncHandler(removeConnection)
-);
 
 export default router;
